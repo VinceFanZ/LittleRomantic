b7e5f302a2e87970fb5833d4c35d18820ec7fff2
vince <vincefanz@gmail.com>
2018-02-26 23:55:38 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,15 @@ const viewWidth = window.innerWidth || document.documentElement.clientWidth
 const viewHeight = window.innerHeight || document.documentElement.clientHeight
 const ctx = createCanvas(viewWidth, viewHeight)
 
+let i = 0
+while (i < 100) {
+  const x = Math.random() * viewWidth
+  const y = Math.random() * viewHeight
+  const particle = new Particle(x, y)
+  particle.render(ctx)
+  ++i
+}
+
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept(() => {
     // or use this instead of dispose()
