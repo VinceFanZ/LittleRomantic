2800a90a0dcd875a34a72b4cbda5fd512b076c13
vince <vincefanz@gmail.com>
2018-02-28 01:03:32 +0800

    不能爆炸的烟花

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,32 @@
 import 'normalize.css'
 import createCanvas from './create-canvas'
 import Firework from './Firework'
-import Particle from './Particle'
 
 const viewWidth = window.innerWidth || document.documentElement.clientWidth
 const viewHeight = window.innerHeight || document.documentElement.clientHeight
 const ctx = createCanvas(viewWidth, viewHeight)
 
-let i = 0
-while (i < 100) {
-  const x = Math.random() * viewWidth
-  const y = Math.random() * viewHeight
-  const particle = new Particle(x, y)
-  particle.render(ctx)
-  ++i
+ctx.fillStyle = 'hsla(210, 60%, 5%, 0.9)'
+ctx.fillRect(0, 0, viewWidth, viewHeight)
+
+const fireworkTimeRange: { min: number; max: number } = { min: 30, max: 60 }
+let fireworkTime = Math.floor(Math.random() * (fireworkTimeRange.max - fireworkTimeRange.min) + fireworkTimeRange.min)
+const fireworks: Array<Firework> = []
+
+loop()
+
+function loop () {
+  window.requestAnimationFrame(loop)
+  ctx.clearRect(0, 0, viewWidth, viewHeight)
+
+  if (--fireworkTime <= 0) {
+    fireworkTime = Math.floor(Math.random() * (fireworkTimeRange.max - fireworkTimeRange.min) + fireworkTimeRange.min)
+    fireworks.push(new Firework())
+  }
+
+  fireworks.forEach((firework, index) => {
+    !firework.render(ctx) && fireworks.splice(index, 1)
+  })
 }
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
